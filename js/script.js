1066c448c975cfbcdf92bec0b073fbf962cfb83d
Paola Garbato <paolagarbato@gmail.com>
2021-03-04 14:10:30 -0300

    Exibindo cep ao pesquisá-lo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 
 
 $(function() {
-    $("#consultar").on("click",  function() {
 
-    var cep = $('#cep').val();
-    var url = "https://viacep.com.br/ws/"+cep+"/json/";
-    
+    $("#cepTitulo").hide();
+    $("#consultar").on("click",  function() {
+        var cep = $('#cep').val();
+        var url = "https://viacep.com.br/ws/"+cep+"/json/";
+        $('#cepTitulo').show();
+
         $.ajax({
             url: url,
             type: "GET",
             success: function(response) {
                 console.log(response);
+                $("#cepTitulo").html(response.cep);
                 $('#ddd').html(response.ddd);
                 $('#rua').html(response.logradouro);
                 $('#bairro').html(response.bairro);
